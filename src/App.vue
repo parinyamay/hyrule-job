d2d9ceebc6175afb261da7b9215d8a75721fe3cf
ParinyaWante <parinyaw@8interactive.co.th>
2023-03-20 14:33:53 +0700

    lesson: Lessib 3 - Composition API & TypeScript

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,43 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div class="app">
+    <p>{{jobs[0].location}}</p>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import { defineComponent, reactive, ref, toRefs } from 'vue'
+import Job from './types/Job'
 
 export default defineComponent({
-  name: "App",
-  components: {
-    HelloWorld,
-  },
-});
-</script>
+  name: 'App',
+  components: {},
+  setup() {
+    // Reactive style
+    //########################################################################
+    // const state = reactive({
+    //   name: 'Link',
+    //   age: 25 as string | number,
+    // })
+    // state.name = 999 // cannot change type
+    // state.age = false
+    // return { ...toRefs(state) }
+    //########################################################################
+    // Ref style
+    //########################################################################
+    // const name = ref('Link')
+    // const age = ref<number | string>(25)
+    // return { name, age }
+    //########################################################################
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+    const jobs = ref<Job[]>([
+      { title: 'fram worker', location: 'lon lon ranch', salary: 30000, id: '1'},
+      { title: 'quarryman', location: 'death mountain', salary: 40000, id: '2'},
+      { title: 'flute player', location: 'the lost woods', salary: 35000, id: '3'},
+      { title: 'fisherman', location: 'lake hylia', salary: 21000, id: '4'},
+      { title: 'prison guard', location: 'gerudo valley', salary: 32000, id: '5'},
+    ])
+
+    return { jobs }
+  },
+})
+</script>
