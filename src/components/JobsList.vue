84e85977327f3f20e49d93c1546ef6864b20f105
ParinyaWante <parinyaw@8interactive.co.th>
2023-03-20 15:13:39 +0700

    lesson: Lesson 6 - Computed Values

--- src/components/JobsList.vue
+++ src/components/JobsList.vue
@@ -2,7 +2,7 @@
   <div class="job-list">
     <p>Ordered by {{ order }}</p>
     <ul>
-      <li v-for="job in jobs" :key="job.id">
+      <li v-for="job in orderedJobs" :key="job.id">
         <h2>{{ job.title }} in {{ job.location }}</h2>
         <div class="salary">
           <p>{{ job.salary }}</p>
@@ -20,7 +20,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from 'vue'
+import { computed, defineComponent, PropType } from 'vue'
 import Job from '@/types/Job'
 import OrderTerm from '@/types/OrderTerm'
 
@@ -35,9 +35,15 @@ export default defineComponent({
       type: String as PropType<OrderTerm>,
     },
   },
-  // setup() {
+  setup(props) {
+    const orderedJobs = computed(() => {
+      return [...props.jobs].sort((a: Job, b: Job) => {
+        return a[props.order] > b[props.order] ? 1 : -1
+      })
+    })
 
-  // },
+    return { orderedJobs }
+  },
 })
 </script>
 
